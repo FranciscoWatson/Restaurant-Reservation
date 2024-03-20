343595d4de181455f1f519f69de88d5b24bf5dd0
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-03-20 14:45:57 -0300

    Create Models for the Database Tables

---
+++ RestaurantReservation.Db/Models/Table.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReservation.Db.Models
+{
+    public class Table
+    {
+        public int TableId { get; set; }
+        public int RestaurantId { get; set; }
+        public int Capacity { get; set; }
+        public List<Reservation> Reservations { get; set; }
+    }
+}
