07b6e336c51709263284c481ca629a343306d43c
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-03-20 12:38:51 -0300

    Create DBContext for RestaurantReservation

---
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace RestaurantReservation.Db
+{
+    public class RestaurantReservationDbContext : DbContext
+    {
+        private readonly string ConnectionString;
+
+        public RestaurantReservationDbContext(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        } 
+
+    }
+}
--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using RestaurantReservation.Db;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+var connectionString = configuration.GetConnectionString("SqlServerConnection");
+
+using (var dbContext = new RestaurantReservationDbContext(connectionString!))
+{
+
+}
