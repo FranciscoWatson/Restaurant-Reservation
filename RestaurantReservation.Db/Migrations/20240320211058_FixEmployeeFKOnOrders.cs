27af51d78e2d3e4cca916e9c5950928ae01c3421
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-03-20 21:48:04 -0300

    Add Migrations for Database's Tables Creation

---
+++ RestaurantReservation.Db/Migrations/20240320211058_FixEmployeeFKOnOrders.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixEmployeeFKOnOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_EmployeeId",
+                table: "Orders",
+                column: "EmployeeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Employees_EmployeeId",
+                table: "Orders",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "EmployeeId",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Employees_EmployeeId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_EmployeeId",
+                table: "Orders");
+        }
+    }
+}
