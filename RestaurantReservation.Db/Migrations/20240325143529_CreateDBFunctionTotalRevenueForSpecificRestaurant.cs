32f50876521012a8061994b94e10d806e2c07e6c
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-03-25 12:42:30 -0300

    Add database function to CalculateTotalRevenue of a Restaurant via Migrations

---
+++ RestaurantReservation.Db/Migrations/20240325143529_CreateDBFunctionTotalRevenueForSpecificRestaurant.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDBFunctionTotalRevenueForSpecificRestaurant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                    CREATE OR ALTER FUNCTION CalculateTotalRevenue(@RestaurantId INT)
+                    RETURNS DECIMAL(18,2)
+                    AS
+                    BEGIN
+                        RETURN (
+                            SELECT SUM(o.TotalAmount) 
+                            FROM Orders o
+                            INNER JOIN Reservations r ON o.ReservationId = r.ReservationId
+                            WHERE r.RestaurantId = @RestaurantId
+                        )
+                    END
+            ");
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTION IF EXISTS CalculateTotalRevenue");
+        }
+    }
+}
