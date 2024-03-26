f9541cdb88114b42acccc8fca1ed249c051e41aa
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-03-26 08:24:48 -0300

    Add Trigger through Migrations to Calculate Total Amount for the Orders

---
+++ RestaurantReservation.Db/Migrations/20240326111737_AddTotalAmountTrigger.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReservation.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTotalAmountTrigger : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE OR ALTER TRIGGER OrderTotalAmount
+                ON OrderItems
+                AFTER INSERT, UPDATE, DELETE
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+
+                    DECLARE @AffectedOrders TABLE (OrderId INT);
+
+                    INSERT INTO @AffectedOrders
+                    SELECT OrderId FROM INSERTED
+                    UNION
+                    SELECT OrderId FROM DELETED;
+
+                    UPDATE Orders
+                    SET TotalAmount = COALESCE((
+                        SELECT SUM(oi.Quantity * mi.Price)
+                        FROM OrderItems oi
+                        INNER JOIN MenuItems mi ON oi.ItemId = mi.ItemId
+                        WHERE oi.OrderId = Orders.OrderId
+                    ), 0)
+                    WHERE Orders.OrderId IN (SELECT OrderId FROM @AffectedOrders);
+                END;
+            ");
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP TRIGGER IF EXISTS UpdateOrderTotalAmount;");
+        }
+    }
+}
