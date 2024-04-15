28cdf19d1ce1cdc2a7e2d7f06cfd64a263e8c85d
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-15 14:00:41 -0300

    Modify Update method on OrderItemRepository

--- RestaurantReservation.Db/Repositories/OrderItemRepository/IOrderItemRepository.cs
+++ RestaurantReservation.Db/Repositories/OrderItemRepository/IOrderItemRepository.cs
@@ -10,7 +10,7 @@ namespace RestaurantReservation.Db.Repositories.OrderItemRepository
     public interface IOrderItemRepository
     {
         Task<IEnumerable<OrderItem>> GetAllAsync();
-        Task<OrderItem> GetByIdAsync(int orderItemId);
+        Task<OrderItem?> GetByIdAsync(int orderItemId);
         Task AddAsync(OrderItem orderItem);
         Task UpdateAsync(OrderItem updatedOrderItem);
         Task DeleteAsync(int orderItemId);
