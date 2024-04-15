46245d72418fd31eb30ac0c1f9ea516684cc4f0d
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-15 11:20:48 -0300

    Add Order DTOs

---
+++ RestaurantReservation.API/DTOs/OrderDTOs/OrderForCreationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RestaurantReservation.API.DTOs.OrderDTOs;
+
+public class OrderForCreationDto
+{
+    public int ReservationId { get; set; }
+    public int EmployeeId { get; set; }
+    public DateTime OrderDate { get; set; }
+}
