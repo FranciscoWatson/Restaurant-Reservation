ae9a787f65659f02fccab12629f19c8fa0cdc600
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-15 11:21:18 -0300

    Add Order Profile

---
+++ RestaurantReservation.API/Profiles/OrderProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.DTOs.OrderDTOs;
+using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.API.Profiles;
+
+public class OrderProfile : Profile
+{
+    public OrderProfile()
+    {
+        CreateMap<Order, OrderDto>().ReverseMap();
+        CreateMap<OrderForUpdateDto, Order>().ReverseMap();
+        CreateMap<Order, OrderForCreationDto>().ReverseMap();
+    }
+}
