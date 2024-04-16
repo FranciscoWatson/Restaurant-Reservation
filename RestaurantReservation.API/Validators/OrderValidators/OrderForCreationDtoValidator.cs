5a9a3dd19f7f6cc300fe4e4df4277f73e0ac3d49
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-16 16:12:16 -0300

    Add Order Validators

---
+++ RestaurantReservation.API/Validators/OrderValidators/OrderForCreationDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.DTOs.OrderDTOs;
+
+namespace RestaurantReservation.API.Validators.OrderValidators;
+
+public class OrderForCreationDtoValidator : AbstractValidator<OrderForCreationDto>
+{
+    public OrderForCreationDtoValidator()
+    {
+        RuleFor(order => order.ReservationId).ValidateEntityId("Reservation ID");
+        RuleFor(order => order.EmployeeId).ValidateEntityId("Employee ID");
+        RuleFor(order => order.OrderDate).NotEmpty();
+    }
+    
+}
---
+++ RestaurantReservation.API/Validators/OrderValidators/OrderForUpdateDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.DTOs.OrderDTOs;
+
+namespace RestaurantReservation.API.Validators.OrderValidators;
+
+public class OrderForUpdateDtoValidator : AbstractValidator<OrderForUpdateDto>
+{
+    public OrderForUpdateDtoValidator()
+    {
+        RuleFor(order => order.ReservationId).ValidateEntityId("Reservation ID");
+        RuleFor(order => order.EmployeeId).ValidateEntityId("Employee ID");
+        RuleFor(order => order.OrderDate).NotEmpty();
+    }
+}
