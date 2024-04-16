379aec001a886d9cf24d77e2595b90ae90015289
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-16 16:15:23 -0300

    Add OrderItem Validators

---
+++ RestaurantReservation.API/Validators/OrderItemValidators/OrderItemForUpdateDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.DTOs.OrderItemDTOs;
+
+namespace RestaurantReservation.API.Validators.OrderItemValidators;
+
+public class OrderItemForUpdateDtoValidator : AbstractValidator<OrderItemForUpdateDto>
+{
+    public OrderItemForUpdateDtoValidator()
+    {
+        RuleFor(orderItem => orderItem.OrderId).ValidateEntityId("Order ID");
+        RuleFor(orderItem => orderItem.ItemId).ValidateEntityId("Item ID");
+        RuleFor(orderItem => orderItem.Quantity).NotEmpty().GreaterThan(0);
+    }
+}
