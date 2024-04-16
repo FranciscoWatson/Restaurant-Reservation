3c8929e44645458a9e2c4233c65baaf3b809b6bb
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-16 16:19:28 -0300

    Add Reservation Validators

---
+++ RestaurantReservation.API/Validators/ReservationValidators/ReservationForCreationDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.DTOs.ReservationDto;
+
+namespace RestaurantReservation.API.Validators.ReservationValidators;
+
+public class ReservationForCreationDtoValidator : AbstractValidator<ReservationDtoForCreation>
+{
+    public ReservationForCreationDtoValidator()
+    {
+        RuleFor(reservation => reservation.CustomerId).ValidateEntityId("Customer ID");
+        RuleFor(reservation => reservation.RestaurantId).ValidateEntityId("Restaurant ID");
+        RuleFor(reservation => reservation.TableId).ValidateEntityId("Table ID");
+        RuleFor(reservation => reservation.ReservationDate).NotEmpty();
+        RuleFor(reservation => reservation.PartySize).NotEmpty().GreaterThan(0);
+    }
+}
---
+++ RestaurantReservation.API/Validators/ReservationValidators/ReservationForUpdateDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.DTOs.ReservationDto;
+
+namespace RestaurantReservation.API.Validators.ReservationValidators;
+
+public class ReservationForUpdateDtoValidator : AbstractValidator<ReservationForUpdateDto>
+{
+    public ReservationForUpdateDtoValidator()
+    {
+        RuleFor(reservation => reservation.CustomerId).ValidateEntityId("Customer ID");
+        RuleFor(reservation => reservation.RestaurantId).ValidateEntityId("Restaurant ID");
+        RuleFor(reservation => reservation.TableId).ValidateEntityId("Table ID");
+        RuleFor(reservation => reservation.ReservationDate).NotEmpty();
+        RuleFor(reservation => reservation.PartySize).NotEmpty().GreaterThan(0);
+        
+    }
+}
