8b2532028caee40a3e814c887c71911a43420f9f
FranciscoWatson <63161056+FranciscoWatson@users.noreply.github.com>
2024-04-16 16:27:52 -0300

    Add Table Validators

---
+++ RestaurantReservation.API/Validators/TableValidators/TableForCreationDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.DTOs.TableDTOs;
+
+namespace RestaurantReservation.API.Validators.TableValidators;
+
+public class TableForCreationDtoValidator : AbstractValidator<TableForCreationDto>
+{
+    public TableForCreationDtoValidator()
+    {
+        RuleFor(table => table.RestaurantId).NotEmpty().GreaterThan(0);
+        RuleFor(table => table.Capacity).NotEmpty().GreaterThan(0);
+    }
+}
---
+++ RestaurantReservation.API/Validators/TableValidators/TableForUpdateDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.DTOs.TableDTOs;
+
+namespace RestaurantReservation.API.Validators.TableValidators;
+
+public class TableForUpdateDtoValidator : AbstractValidator<TableForUpdateDto>
+{
+    public TableForUpdateDtoValidator()
+    {
+        RuleFor(table => table.RestaurantId).GreaterThan(0);
+        RuleFor(table => table.Capacity).NotEmpty().GreaterThan(0);
+    } 
+}
